d60d96747d53f4c75476377798d5595ea0c57bfa
ErkiD <erkidorbek@gmail.com>
2024-01-26 03:29:46 +0200

    add home component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router'
 import { ArticleComponent } from './article/article.component'
 import { TableComponent } from './table/table.component'
+import { HomeComponent } from './home/home.component'
 
 export const routes: Routes = [
-  { path: 'article', component: ArticleComponent },
+  { path: '', component: HomeComponent },
+  { path: 'article', pathMatch: "full", component: ArticleComponent },
   { path: 'article/:articleId', component: ArticleComponent },
   { path: 'table', component: TableComponent }
 ]
